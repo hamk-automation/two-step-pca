0124016866b8105a4d1f0bb3ed7c98d859f8c9d8
Gradoni <igortrotskii@gmail.com>
2018-08-20 10:23:44 +0300

    testing on the TE simulation

--- test.py
+++ test.py
@@ -1,26 +1,71 @@
 import two_step_pca as TS_PCA 
 import pandas as pd 
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from pathlib import Path
+from os import listdir
+import numpy as np
 
-df = pd.read_csv('clean_data.csv', index_col=0, sep=";")
-#limiting the amount of data, so you can check at least something visually
-#it is a litle bit complicated to do then you have 20 columns initially and even more after time shifting (a lot more)
-data = df[df.columns.values[1:6]]
-np_data = data[:500].values
-U, A, tildaX = TS_PCA.fit_transform(np_data, 6, 50)
-print("U.shape: {}  A.shape: {}  tildaX.shape: {}".format(U.shape, A.shape, tildaX.shape))
+def load_simulation_data(folder):
+    """
+    Loads csv files from "folder" as numpy arrays
+    Arguments:
+        folder - name of the folder where the data is located
+    Returns:
+        sims - dictionary {file_name : numpy array with loaded csv}
+    """
+    files_list = listdir(folder)
+    sims = {}
+    #load all csv files as df into the sims list
+    for file in files_list:
+        path = folder + '/' + file
+        sims.update({file[:-4] :pd.read_csv(path, sep=",").values})
+    #remove info about disturbance
+    sims.pop('disturbance', None)
+    return sims
 
+#load all the files from 'Faults' folder
+data_list = load_simulation_data('Faults')
+#get training data from the list
+train_data = data_list['no_disturbance']
+#training - you only need A_train values from this function, but we still fetching them all to test dimensionality
+U_train, A_train, tildaX_train = TS_PCA.fit_transform(train_data, 1, 200)
+print("U_train.shape: {}  A_train.shape: {}  tildaX_train.shape: {}".format(U_train.shape, A_train.shape, tildaX_train.shape))
+#creating scaler 
 scaler = StandardScaler()
-U = scaler.fit_transform(U)
-print("U: \n",U[:5])
+scaler.fit(U_train)
+#testing
+test_data_dict = {}
 
-T, P, E, var_explained = TS_PCA.pca(U)
-print("T.shape: {} P.shape: P{} E.shape: {} ".format(T.shape, P.shape, E.shape))
-print("Variance explained: ", var_explained)
-T_l, E_l, T_rest = TS_PCA.dividePCs(T, E, var_explained, 0.8)
-print("T_l.shape: {} E_l.shape: {} T_rest.shape: {}".format(T_l.shape, E_l.shape, T_rest.shape))
-T2 = TS_PCA.computeT2(T_l, E_l)
-print("Hotelling's T2 metric: \n", T2[:5])
-SPE = TS_PCA.computeSPE(T_rest)
-print("SPE: \n", SPE[:5])
+#big loop for big guys
+#compute innovation component based on dynamic part for "ideal" data
+#then use pca on innovation component and calcualte metrics 
+#and in the end put all those into a dict for later plotting
+for key in data_list:
+    tilda_test_X = TS_PCA.shift_data(data_list[key], 1) 
+    U_test = data_list[key][1:] - np.dot(tilda_test_X, A_train) #[1:] because shift_data had lag parameter of 1, so we need to remove first element for allignment
+    U_test = scaler.transform(U_test)
+    T, P, E, var_explained = TS_PCA.pca(U_test)
+    T_l, E_l, T_rest = TS_PCA.dividePCs(T, E, var_explained, 0.8)
+    T2 = TS_PCA.computeT2(T_l, E_l)
+    SPE = TS_PCA.computeSPE(T_rest)
+    test_data_dict.update({key: [T2, SPE]})
 
+
+#get disturbance
+disturbance = pd.read_csv('Faults/disturbance.csv', sep=",").values
+#warning: during the fault 6 reactor shuts down shortly after disturbance rising, but the disturbance graph is for the full 
+#simulation - that is the reason for strange graph axes
+#second warning: obviously there is no disturbance for "no_disturbance" example, it just lazy me, who didnt want to make anything with it
+for key in test_data_dict:
+    plt.title(key)
+    plt.subplot(3, 1, 1)
+    plt.ylabel('T2: ' + key)
+    plt.plot(test_data_dict[key][0])
+    plt.subplot(3, 1 , 2)
+    plt.ylabel('SPE: ' + key)
+    plt.plot(test_data_dict[key][1])
+    plt.subplot(3, 1, 3)
+    plt.ylabel('Disturbance')
+    plt.plot(disturbance)
+    plt.show()
